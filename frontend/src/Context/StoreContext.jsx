2bf7f90001b01663d512e6a0d3c1ee6da610d295
Ishika Deshpande <ishikadeshpande816@gmail.com>
2025-01-11 14:46:49 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = 'https://dishdelight-78ww.onrender.com"
+    const url = 'https://dishdelight-78ww.onrender.com";
 
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
