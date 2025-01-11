200625046aeb660783721e8ed588f66e723fbaf3
Ishika Deshpande <ishikadeshpande816@gmail.com>
2025-01-11 15:59:34 +0530

    Update Orders.jsx

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Orders.css'
 import { toast } from 'react-toastify';
 import axios from 'axios';
-import { assets,currency,url } from '../../assets/assets';
+import { assets , url , currency } from '../../assets/assets';
 
 const Order = ({url}) => {
 
